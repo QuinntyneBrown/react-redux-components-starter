cd4689de152d688714e90ca936c7d436cef730a4
Quinntyne Brown <quinntynebrown@gmail.com>
2016-08-09 22:33:21 -0400

    components

--- src/components/photo-gallery/photo-gallery.tsx
+++ src/components/photo-gallery/photo-gallery.tsx
@@ -1,1 +1,25 @@
-﻿
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+interface Props {
+
+}
+
+interface State {
+
+}
+
+
+class PhotoGallery extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default PhotoGallery;
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,2 @@
-﻿
+﻿import Hero from "./components/hero/hero";
+import PhotoGallery from "./components/photo-gallery/photo-gallery";
